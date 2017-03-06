f28386c0d6fc70c7404b5dc3774989fd9eaf8072
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-03-05 21:21:18 -0300

    projeto para aulas de simulador de texturas em diversos ambientes

--- simulador.php
+++ simulador.php
@@ -9,6 +9,8 @@
 
         <div id="simulador">
 
+            <h2>Simulador de Ambientes</h2>
+
             <div id="quadro">
                 <div id="ambienteFoto"></div>
             </div>
@@ -16,6 +18,68 @@
             <div id="paleta">
                 <figure style="background:url('images/catalogo/grafiatos/BT01_grafiato_branco_arizona.jpg')" onclick="$.Simulador('BT01_grafiato_branco_arizona.jpg');" title="BT01 grafiato branco arizona"></figure>
                 <figure style="background:url('images/catalogo/grafiatos/BT02_grafiato_cabelo_de_anjo.jpg')" onclick="$.Simulador('BT02_grafiato_cabelo_de_anjo.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT03_grafiato_areia.jpg')" onclick="$.Simulador('BT03_grafiato_areia.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT04_grafiato_areia_sirena.jpg')" onclick="$.Simulador('BT04_grafiato_areia_sirena.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT05_grafiato_bronze_de_milao.jpg')" onclick="$.Simulador('BT05_grafiato_bronze_de_milao.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT06_grafiato_marfim_classico.jpg')" onclick="$.Simulador('BT06_grafiato_marfim_classico.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT07_grafiato_pato_marinho.jpg')" onclick="$.Simulador('BT07_grafiato_pato_marinho.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT08_grafiato_cominho.jpg')" onclick="$.Simulador('BT08_grafiato_cominho.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT09_grafiato_palha.jpg')" onclick="$.Simulador('BT09_grafiato_palha.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT10_grafiato_areia_da_praia.jpg')" onclick="$.Simulador('BT10_grafiato_areia_da_praia.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT11_grafiato_nostalgia.jpg')" onclick="$.Simulador('BT11_grafiato_nostalgia.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT12_grafiato_manuscrito.jpg')" onclick="$.Simulador('BT12_grafiato_manuscrito.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT13_grafiato_doce_mel.jpg')" onclick="$.Simulador('BT13_grafiato_doce_mel.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT14_grafiato_creme_holandes.jpg')" onclick="$.Simulador('BT14_grafiato_creme_holandes.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT15_grafiato_areia_de_marte.jpg')" onclick="$.Simulador('BT15_grafiato_areia_de_marte.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT17_grafiato_laranja_mecanica.jpg')" onclick="$.Simulador('BT17_grafiato_laranja_mecanica.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT18_grafiato_golden_sand.jpg')" onclick="$.Simulador('BT18_grafiato_golden_sand.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT19_grafiato_gold.jpg')" onclick="$.Simulador('BT19_grafiato_gold.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT20_grafiato_extratosfera.jpg')" onclick="$.Simulador('BT20_grafiato_extratosfera.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT21_grafiato_marfim.jpg')" onclick="$.Simulador('BT21_grafiato_marfim.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT22_grafiato_cromo.jpg')" onclick="$.Simulador('BT22_grafiato_cromo.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT24_grafiato_laranja.jpg')" onclick="$.Simulador('BT24_grafiato_laranja.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT25_grafiato_saigon_summer.jpg')" onclick="$.Simulador('BT25_grafiato_saigon_summer.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT26_grafiato_ocre_alaranjado.jpg')" onclick="$.Simulador('BT26_grafiato_ocre_alaranjado.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT27_grafiato_vermelho_terra.jpg')" onclick="$.Simulador('BT27_grafiato_vermelho_terra.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT28_grafiato_vermelho_carmin.jpg')" onclick="$.Simulador('BT28_grafiato_vermelho_carmin.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT29_grafiato_viena.jpg')" onclick="$.Simulador('BT29_grafiato_viena.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT30_grafiato_tropical.jpg')" onclick="$.Simulador('BT30_grafiato_tropical.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT31_grafiato_gelo.jpg')" onclick="$.Simulador('BT31_grafiato_gelo.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT32_grafiato_abobora.jpg')" onclick="$.Simulador('BT32_grafiato_abobora.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT33_grafiato_dakar.jpg')" onclick="$.Simulador('BT33_grafiato_dakar.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT34_grafiato_oliva.jpg')" onclick="$.Simulador('BT34_grafiato_oliva.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT35_grafiato_laranja_forte.jpg')" onclick="$.Simulador('BT35_grafiato_laranja_forte.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT36_grafiato_tijolo.jpg')" onclick="$.Simulador('BT36_grafiato_tijolo.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT37_grafiato_verde_musgo.jpg')" onclick="$.Simulador('BT37_grafiato_verde_musgo.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT38_grafiato_caqui.jpg')" onclick="$.Simulador('BT38_grafiato_caqui.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT39_grafiato_laranja_verao-.jpg')" onclick="$.Simulador('BT39_grafiato_laranja_verao.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT40_grafiato_ferrugem.jpg')" onclick="$.Simulador('BT40_grafiato_ferrugem.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT41_grafiato_cobre.jpg')" onclick="$.Simulador('BT41_grafiato_cobre.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT43_grafiato_bronze.jpg')" onclick="$.Simulador('BT43_grafiato_bronze.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT44_grafiato_vermelho_sangue.jpg')" onclick="$.Simulador('BT44_grafiato_vermelho_sangue.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT45_grafiato_orquidea_escura.jpg')" onclick="$.Simulador('BT45_grafiato_orquidea_escura.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT46_grafiato_chumbo.jpg')" onclick="$.Simulador('BT46_grafiato_chumbo.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT48_grafiato_vermelho.jpg')" onclick="$.Simulador('BT48_grafiato_vermelho.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT49_grafiato_azul_aco_claro.jpg')" onclick="$.Simulador('BT49_grafiato_azul_aco_claro.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT50_grafiato_cinza_fosco.jpg')" onclick="$.Simulador('BT50_grafiato_cinza_fosco.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT51_grafiato_prata.jpg')" onclick="$.Simulador('BT51_grafiato_prata.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT52_grafiato_caqui_escuro.jpg')" onclick="$.Simulador('BT52_grafiato_caqui_escuro.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+
+                <figure style="background:url('images/catalogo/grafiatos/BT53_grafiato_azul_navy.jpg')" onclick="$.Simulador('BT53_grafiato_azul_navy.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT54_grafiato_azul_cobalto.jpg')" onclick="$.Simulador('BT54_grafiato_azul_cobalto.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT55_grafiato_cinza.jpg')" onclick="$.Simulador('BT55_grafiato_cinza.jpg');" title="BT01 grafiato branco arizona"></figure>
+                <figure style="background:url('images/catalogo/grafiatos/BT56_grafiato_quantum.jpg')" onclick="$.Simulador('BT56_grafiato_quantum.jpg');" title="BT02 grafiato cabelo de anjo"></figure>
             </div>
 
             <div id="ambiente">
